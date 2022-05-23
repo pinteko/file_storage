8b03de3ae87911581f0cd008bbaf9d9999836a29
Kirill Korsuk <korsukkirill@gmail.com>
2022-05-23 22:13:38 +0300

    update

--- src/main/java/geekbrains/Main.java
+++ src/main/java/geekbrains/Main.java
@@ -1,5 +1,8 @@
-package geekbrains;public class Main {
+package geekbrains;
+
+public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
 }
